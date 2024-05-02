0793d0d8b161ae22e84559ad1ef6fac796530787
Pedro Nalvarte <impedro2406@gmail.com>
2024-05-01 21:53:18 -0500

    ft: sedes v1

--- index.js
+++ index.js
@@ -38,6 +38,18 @@ app.post('/reset/:user/:password', (req, res) => {
         })
 });
 
+app.post('/registerSede/:dni', (req, res) => {
+
+    registerSede(req.body, req.params.dni)
+        .then((result) => {
+
+            console.log(result.res);
+            res.send(result.res);
+
+        })
+
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
@@ -92,6 +104,34 @@ const resetPassword = async (user, password) => {
     const result = res.rows[0];
 
     await client.end();
+
+    return result;
+}
+
+const registerSede = async (sede, dni) => {
+
+    const client = new Client({
+        user: "omodygym_user",
+        host: "dpg-cocr9amv3ddc739ki7b0-a.oregon-postgres.render.com",
+        database: "omodygym",
+        password: "9sAnVEwzwYzR1GMdsET5UQo7XzYjcrup",
+        port: 5432,
+        ssl: {
+            rejectUnauthorizedL: false,
+        }
+    });
+
+    await client.connect();
+
+    const res = await client.query(`INSERT INTO public.sede(
+        nombre_sede, abreviacion, direccion, fecha_creacion, fecha_modificacion, estado, usuario_modificacion)
+        VALUES ('${sede.nombreSede}', '${sede.abreviacion}', '${sede.direccion}', CURRENT_DATE, CURRENT_DATE, 'A', ${dni}) RETURNING true as res;`);
+
+    const result = res.rows[0];
+
+    await client.end();
+
+    console.log("result = ", result)
 
     return result;
 }
