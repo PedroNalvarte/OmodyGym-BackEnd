c9875c9fb59eeaca29a0f050d7a40a0646ca2d87
NicoleZevallos <nicole_zevallos1@usmp.pe>
2024-05-04 08:51:30 -0500

    ft: clients filtered

--- index.js
+++ index.js
@@ -56,8 +56,14 @@ app.post('/getClients', (req, res) => {
         console.error("Error al obtener las membresías:", error);
     });
 
-  
-    
+});
+app.post('/getClientsFiltered/:input', (req, res) => {
+    const input = req.params.input;
+    getClientsFiltered(input).then((result) => {
+        res.send(result);
+    }).catch((error) => {
+        console.error("Error al obtener las membresías:", error);
+    });
 
 });
 app.post('/getMemberships', (req, res) => {
@@ -248,7 +254,7 @@ const getClients = async () => {
         }
     });
     await client.connect();
-    const result = await  client.query(`SELECT A.id_persona, A.nombre_1, A.apellido_1, A.apellido_2, EXTRACT(YEAR FROM AGE(fecha_nacimiento)) AS edad, a.telefono, d.nombre_sede, e.nombre as membresia  FROM persona a INNER JOIN CONTRATO b ON a.ID_PERSONA = b.ID_PERSONA INNER JOIN TIPO_PERSONA c ON b.ID_TIPO_PERSONA = c.ID_TIPO_PERSONA INNER JOIN SEDE d ON b.ID_SEDE = d.ID_SEDE INNER JOIN MEMBRESIA e ON b.ID_MEMBRESIA = e.ID_MEMBRESIA WHERE C.TIPO_PERSONA = 'C' AND a.ESTADO = 'A';`);
+    const result = await  client.query(`SELECT A.id_persona, A.nombre_1, A.apellido_1, A.apellido_2, EXTRACT(YEAR FROM AGE(fecha_nacimiento)) AS edad, a.telefono, d.nombre_sede, e.nombre as membresia, b.fecha_fin  FROM persona a INNER JOIN CONTRATO b ON a.ID_PERSONA = b.ID_PERSONA INNER JOIN TIPO_PERSONA c ON b.ID_TIPO_PERSONA = c.ID_TIPO_PERSONA INNER JOIN SEDE d ON b.ID_SEDE = d.ID_SEDE INNER JOIN MEMBRESIA e ON b.ID_MEMBRESIA = e.ID_MEMBRESIA WHERE c.TIPO_PERSONA = 'C' AND a.ESTADO = 'A';`);
     const clientes = result.rows.map(row => ({
         Id: row.id_persona,
         nombre: row.nombre_1,
@@ -257,8 +263,36 @@ const getClients = async () => {
         edad: row.edad,
         telefono: row.telefono,
         sede: row.nombre_sede,
-        membresia: row.membresia
+        membresia: row.membresia,
+        fechafin: row.fecha_fin
+    }));
+    await client.end();
+    return clientes;
+}
 
+const getClientsFiltered = async (input) => {
+    const client = new Client({
+        user: "omodygym_user",
+        host: "dpg-cocr9amv3ddc739ki7b0-a.oregon-postgres.render.com",
+        database: "omodygym",
+        password: "9sAnVEwzwYzR1GMdsET5UQo7XzYjcrup",
+        port: 5432,
+        ssl: {
+            rejectUnauthorizedL: false,
+        }
+    });
+    await client.connect();
+    const result = await  client.query(`SELECT A.id_persona, A.nombre_1, A.apellido_1, A.apellido_2, EXTRACT(YEAR FROM AGE(fecha_nacimiento)) AS edad, a.telefono, d.nombre_sede, e.nombre as membresia, b.fecha_fin  FROM persona a INNER JOIN CONTRATO b ON a.ID_PERSONA = b.ID_PERSONA INNER JOIN TIPO_PERSONA c ON b.ID_TIPO_PERSONA = c.ID_TIPO_PERSONA INNER JOIN SEDE d ON b.ID_SEDE = d.ID_SEDE INNER JOIN MEMBRESIA e ON b.ID_MEMBRESIA = e.ID_MEMBRESIA WHERE  a.ESTADO = 'A' AND (a.nombre_1 like '%${input}%' || a.apellido_1 like '%${input}%' || a.apellido_2 like '%${input}%' || a.numero_documento_identidad like '%${input}%') ;`);
+    const clientes = result.rows.map(row => ({
+        Id: row.id_persona,
+        nombre: row.nombre_1,
+        apellido1: row.apellido_1,
+        apellido2: row.apellido_2,
+        edad: row.edad,
+        telefono: row.telefono,
+        sede: row.nombre_sede,
+        membresia: row.membresia,
+        fechafin: row.fecha_fin
     }));
     await client.end();
     return clientes;
